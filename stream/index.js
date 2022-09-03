b2ef4bbaa3817fdbd28b04faead44e066b9ca1f9
Budiman Fajar Firdaus <budimanfajarf@gmail.com>
2022-09-03 23:06:26 +0700

    Exercise writeable stream

--- stream/index.js
+++ stream/index.js
@@ -13,17 +13,22 @@ const readableStream = fs.createReadStream(path.resolve(__dirname, 'input.txt'),
     highWaterMark: 15,
 });
 
+const writableStream = fs.createWriteStream(path.resolve(__dirname, 'output.txt'));
+
 readableStream.on('readable', () => {
     try {
         // console.log(`${readableStream.read()}`)
         // console.log(readableStream.read())
-        process.stdout.write(`[${readableStream.read()}]`);
+        // process.stdout.write(`[${readableStream.read()}]`);
+        const text = String(readableStream.read());
+        writableStream.write(`${text}\n`);
     } catch (error) {
         // catch the error when the chunk cannot be read.
     }
 })
 
 readableStream.on('end', () => {
+    writableStream.end();
     console.log('Done');
 });
 
