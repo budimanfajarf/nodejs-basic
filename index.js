44b1310f50d2475870c8c558201cb53b65349230
Budiman Fajar Firdaus <budimanfajarf@gmail.com>
2022-09-03 21:34:23 +0700

    Explore global & process object

--- index.js
+++ index.js
@@ -2,4 +2,15 @@ const message = (name) => {
     console.log(`Hello ${name}`);
 }
 
-message('JavaScript');
+// message('JavaScript');
+
+// console.log(Object.getOwnPropertyNames(global));
+// console.log(process.env);
+// const cpuInformation = process.memoryUsage();
+// console.log(cpuInformation);
+
+const firstName = process.argv[2];
+const lastName = process.argv[3];
+
+// Run in terminal: node index.js budiman fajar
+console.log(`Hello ${firstName} ${lastName}`);
